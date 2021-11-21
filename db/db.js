a92245f660522a14f427526ec2fa25b1f847df6d
Adedotun O. Albert-Ojeikere <dotun1m@gmail.com>
2021-11-21 03:15:54 -0500

    db.js is done

---
+++ db/db.js
@@ -0,0 +1,39 @@
+const uuidvl = require('uuid/vl');
+const util = require('util');
+const fs = require('fs');
+
+const write = util.promisify(fs.writeFile);
+const read = util.promisify(fs.readFile);
+
+class Db {
+    read() {
+        return read('db/db.json', 'utf8');
+    }
+
+    write(note) {
+        return write('db/db.json', JSON.stringify(note));
+    }
+
+    getNotes() {
+        return this.read().then((notes) => {
+            let parsedNotes = [].concat(JSON.parse(notes));
+
+            return parsedNotes;
+        });
+    }
+
+    addNote(note) {
+        const { title, text } = note;
+
+        if (!title || !text) {
+            throw new Error("Note 'title' and 'text' cannot be blank");
+        }
+
+        const newNote = { title, text, id: uuidvl()};
+
+        return this.getNotes()
+            .then((notes) => [...notes, newNote])
+            .then((updateNotes) => this.write(updateNotes))
+            .then(() => newNote);
+    }
+}
