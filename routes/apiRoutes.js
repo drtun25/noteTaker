4fa2cb56cbe87802461636229d60f0e74137276d
Adedotun O. Albert-Ojeikere <dotun1m@gmail.com>
2021-11-24 01:08:40 -0500

    fixing part2

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -35,3 +35,33 @@ app.post('/api/notes', function(req, res) {
     });
     res.send('Thank you for your note!');
   });
+
+
+  // API DELETE Requests
+  app.delete('/api/notes/:id', function(req, res) {
+    // Gets id number of note to delete
+    const deleteNote = req.params.id;
+    console.log(deleteNote);
+
+    fs.readFile('./db/db.json', (err, data) => {
+      if (err) throw err;
+
+ 
+      dbData = JSON.parse(data);
+
+      for (let i = 0; i < dbData.length; i++) {
+        if (dbData[i].id === Number(deleteNote)) {
+          dbData.splice([i], 1);
+        }
+      }
+      console.log(dbData);
+      stringData = JSON.stringify(dbData);
+
+      fs.writeFile('./db/db.json', stringData, (err, data) => {
+        if (err) throw err;
+      });
+    });
+
+    res.status(204).send();
+  });
+};
