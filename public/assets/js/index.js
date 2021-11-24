93adbc11739e7217b0757b9684ef241510032fc6
Adedotun O. Albert-Ojeikere <dotun1m@gmail.com>
2021-11-24 02:54:25 -0500

    added posts

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -3,3 +3,30 @@ const $noteText = $('.textarea-note');
 const $saveNoteBtn = $('.note-new');
 const $newNoteBtn = $('.note-save');
 const $noteList = $('.list-container .list-group');
+
+let activeNote = {};
+
+//get notes from db
+const getNotes = function() {
+  return $.ajax({
+    url: '/api/notes',
+    method: 'GET',
+  });
+};
+
+//save
+const saveNote = function(note) {
+    return $.ajax({
+      url: '/api/notes',
+      data: note,
+      method: 'POST',
+    });
+  };
+
+// delete
+const deleteNote = function(id) {
+    return $.ajax({
+      url: `api/notes/${id}`,
+      method: 'DELETE',
+    });
+  };
